b6f3b5570f7df729195ec5fad5a95bf96272e2e2
jtr109 <lyp_login@outlook.com>
2019-10-06 09:48:35 +0800

    feat(app): add function to decode token

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use jsonwebtoken as jwt;
-use jsonwebtoken::{decode, encode, Algorithm, Header, Validation};
+use jsonwebtoken::{decode, encode, Header, Validation};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
@@ -26,8 +26,32 @@ fn create_claims(sub: &str) -> Claims {
     }
 }
 
-fn encode_token(claims: &Claims, secret: &str) -> jwt::errors::Result<String> {
-    encode(&Header::default(), claims, secret.as_bytes())
+#[derive(Debug)]
+enum AppError {
+    JwtError(jwt::errors::Error),
+}
+
+impl From<jwt::errors::Error> for AppError {
+    fn from(error: jwt::errors::Error) -> Self {
+        AppError::JwtError(error)
+    }
+}
+
+fn encode_token(claims: &Claims, secret: &str) -> Result<String, AppError> {
+    encode(&Header::default(), claims, secret.as_bytes()).map_err(|e| AppError::JwtError(e))
+}
+
+fn decode_token(encoded: &str, secret: &str) -> Result<Claims, AppError> {
+    // decode json web token
+    // 由于目前 token 中不需要 exp, 忽略 exp validation
+    // 方法见: https://github.com/Keats/jsonwebtoken/issues/65
+    let valication = Validation {
+        validate_exp: false,
+        ..Validation::default()
+    };
+    decode::<Claims>(encoded, secret.as_bytes(), &valication)
+        .map(|data| data.claims.into())
+        .map_err(|e| AppError::JwtError(e))
 }
 
 #[cfg(test)]
@@ -47,5 +71,14 @@ mod test {
         let expected = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.9nQevcPZ--Asqxinx5l1pgqRf0tCGR6Wvw_AvVN5CDA";
         let claims = create_claims("1234567890");
         assert_eq!(encode_token(&claims, &secret).unwrap(), expected);
+    }
+
+    #[test]
+    fn test_decode_token() {
+        let encoded = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.9nQevcPZ--Asqxinx5l1pgqRf0tCGR6Wvw_AvVN5CDA";
+        let secret = "your-256-bit-secret";
+        // let claims = decode_token(&encoded, &secret).unwrap();
+        let claims = decode_token(&encoded, &secret).unwrap();
+        assert_eq!(claims.sub, "1234567890");
     }
 }
