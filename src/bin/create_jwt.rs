858df20d49142ad23046a39b7b2fede805d38134
jtr109 <lyp_login@outlook.com>
2019-10-07 11:43:40 +0800

    feat(app): get appid from command line

--- src/bin/create_jwt.rs
+++ src/bin/create_jwt.rs
@@ -1,13 +1,14 @@
 extern crate sgauth;
 
 use sgauth::models::{establish_connection, App};
+use std::env;
 use uuid::Uuid;
 
 // create jwt from app
 fn main() {
-    // let id = Uuid::new_v4();
-    let id = Uuid::parse_str("a3e1d04b629d4faaac5ab7a77b41be39").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let id = Uuid::parse_str(&args[1]).unwrap();
     let conn = establish_connection();
     let app = App::get_by_id(&conn, id).unwrap().unwrap();
-    println!("app token is {:?}", app.encode_jwt().unwrap());
+    println!("app token is\n{}", app.encode_jwt().unwrap());
 }
