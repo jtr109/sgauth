cdbddaa419770b9613d033863b504fde34eb6f8e
jtr109 <lyp_login@outlook.com>
2019-10-10 08:34:53 +0800

    feat(server): add logger

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,17 @@
 extern crate sgauth;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use env_logger;
 use sgauth::handlers::authentication;
 
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     HttpServer::new(|| {
-        App::new().service(web::scope("/app").route("/auth", web::get().to(authentication)))
+        App::new()
+            .wrap(Logger::default())
+            .service(web::scope("/app").route("/auth", web::get().to(authentication)))
     })
     .bind("127.0.0.1:8088")
     .unwrap()
